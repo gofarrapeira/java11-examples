d2d92ba45798f745ed8090cd645a7eee3ca46bf0
Gloria Brandao <gloria.andrade.brandao@gmail.com>
2023-08-26 11:40:18 -0300

    subindo uma alteracao

--- src/main/java/io/github/biezhi/java11/ProjetoVideo.java
+++ src/main/java/io/github/biezhi/java11/ProjetoVideo.java
@@ -11,7 +11,7 @@ package io.github.biezhi.java11;
             video[0].setViews(240000);
             video[0].play();
             System.out.println(video[0].toString());
-
+//adicionando comentario so pra commitar
             Gafanhoto gafanhoto[] = new Gafanhoto[2];
         gafanhoto[0] = new Gafanhoto("Joao", 12, 'M',"Joaozinho", false);
             System.out.println(gafanhoto[0].toString());
